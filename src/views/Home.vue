75bf31a04d85e4ace8f8021dbebf8d1ee1ee4dc6
xjxkakka <815826084@qq.com>
2022-04-18 20:16:51 +0800

    项目初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+      <el-button>中文</el-button>
+    </el-row>
+
   </div>
 </template>
 
